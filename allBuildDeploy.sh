4067efd313f4eda12da802b400341634d5356db5
wxsimon <simon_code@163.com>
2021-11-11 09:08:29 +0800

    修改部署脚本

--- allBuildDeploy.sh
+++ allBuildDeploy.sh
@@ -6,11 +6,11 @@ args=$*
 
 if [[ -z "$args" ]] || [[ $args =~ "2" ]];then
 cd src/cmd/gateway
-go build -o ./Gateway Gateway.go
+go build -o ./gateway Gateway.go
 
 if [[ $args =~ "3" ]];then
     rm -rf publish.tar.gz
-    tar -zcvf publish.tar.gz Gateway
-    $cliDir/mnt/mng/deployMng.sh axj-gateway publish.tar.gz /opt/mng dev-1
+    tar -zcvf publish.tar.gz gateway
+    $cliDir/mnt/mng/deployMng.sh axj-gw publish.tar.gz /opt/mng dev-1
 fi
 fi
