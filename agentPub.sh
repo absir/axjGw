042f5135caab1a204de73b49e091ed825e4ef50f
absir <absir@qq.com>
2021-12-13 19:01:44 +0800

    nice

--- agentPub.sh
+++ agentPub.sh
@@ -10,8 +10,8 @@ rm -rf bin/mips/*
 cp src/bin/agent-mips bin/mips/agent
 cp install/agent.yml bin/mips/
 cp install/mipsRun.sh bin/mips/run.sh
-cd bin/mips
+cd bin
 rm -rf mips.tar.gz
-tar -zcvf mips.tar.gz ./
+tar -zcvf mips.tar.gz -C mips .
 $cliDir/mnt/mng/deployMng.sh agent-mips mips.tar.gz /opt/mng dev-1
 fi
