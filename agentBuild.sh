f8f56b516b1eda9a4e9bef5d4aa59048391a9116
wxsimon <simon_code@163.com>
2024-12-24 13:39:13 +0800

    update

--- agentBuild.sh
+++ agentBuild.sh
@@ -1,60 +1,55 @@
-#!/usr/bin/env bash
+#!/bin/sh
 cd `dirname $0`
 args=$*
 
 mkdir -p src/bin
 
 cd src/bin
-export CGO_ENABLED=0
+CGO_ENABLED=0; export CGO_ENABLED
+echo "args: $args"
 
-if [[ $args =~ "mips" ]];then
-export GOOS=linux
-export GOARCH=mipsle
-export GOMIPS=softfloat
-#export GOARM=5
-#export GODEBUG=madvdontneed=1
-go build -tags wsN,httpN -o ./agent-mips ../cmd/agent/Agent.go
-fi
-
-if [[ $args =~ "arm0" ]];then
-export GOOS=linux
-export GOARCH=arm
-go build -tags wsN,httpN -o ./agent-arm0 ../cmd/agent/Agent.go
-fi
-
-if [[ $args =~ "arm64" ]];then
-export GOOS=linux
-export GOARCH=arm64
-go build -tags wsN,httpN -o ./agent-arm64 ../cmd/agent/Agent.go
-fi
-
-if [[ $args =~ "arm5" ]];then
-export GOOS=linux
-export GOARCH=arm
-export GOARM=5
-go build -tags wsN,httpN -o ./agent-arm5 ../cmd/agent/Agent.go
-fi
-
-if [[ $args =~ "arm7" ]];then
-export GOOS=linux
-export GOARCH=arm
-export GOARM=7
-go build -tags wsN,httpN -o ./agent-arm7 ../cmd/agent/Agent.go
-fi
-
-if [[ $args =~ "linux" ]];then
-export GOOS=linux
-export GOARCH=amd64
-go build -tags wsN -o ./agent-linux ../cmd/agent/Agent.go
-fi
-
-if [[ $args =~ "win" ]];then
-export GOOS=windows
-export GOARCH=amd64
-go build -tags wsN -o ./agent-win.exe ../cmd/agent/Agent.go
-mkdir ../../bin/win
-rm -rf ../../bin/win/*
-cp -rf agent-win.exe ../../bin/win/
-cp -rf ../../install/agent.yml ../../bin/win/
-cp -rf ../../install/win/* ../../bin/win/
-fi
+case "$args" in
+    *mips*)
+        GOOS=linux; export GOOS
+        GOARCH=mipsle; export GOARCH
+        GOMIPS=softfloat; export GOMIPS
+        go build -tags wsN,httpN -o ./agent-mips ../cmd/agent/Agent.go
+        ;;
+    *arm0*)
+        GOOS=linux; export GOOS
+        GOARCH=arm; export GOARCH
+        go build -tags wsN,httpN -o ./agent-arm0 ../cmd/agent/Agent.go
+        ;;
+    *arm64*)
+        GOOS=linux; export GOOS
+        GOARCH=arm64; export GOARCH
+        go build -tags wsN,httpN -o ./agent-arm64 ../cmd/agent/Agent.go
+        ;;
+    *arm5*)
+        GOOS=linux; export GOOS
+        GOARCH=arm; export GOARCH
+        GOARM=5; export GOARM
+        go build -tags wsN,httpN -o ./agent-arm5 ../cmd/agent/Agent.go
+        ;;
+    *arm7*)
+        GOOS=linux; export GOOS
+        GOARCH=arm; export GOARCH
+        GOARM=7; export GOARM
+        go build -tags wsN,httpN -o ./agent-arm7 ../cmd/agent/Agent.go
+        ;;
+    *linux*)
+        GOOS=linux; export GOOS
+        GOARCH=amd64; export GOARCH
+        go build -tags wsN -o ./agent-linux ../cmd/agent/Agent.go
+        ;;
+    *win*)
+        GOOS=windows; export GOOS
+        GOARCH=amd64; export GOARCH
+        go build -tags wsN -o ./agent-win.exe ../cmd/agent/Agent.go
+        mkdir ../../bin/win
+        rm -rf ../../bin/win/*
+        cp -rf agent-win.exe ../../bin/win/
+        cp -rf ../../install/agent.yml ../../bin/win/
+        cp -rf ../../install/win/* ../../bin/win/
+        ;;
+esac
